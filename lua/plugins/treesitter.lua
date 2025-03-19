1c4e342e436cc8f5bb45be8d193f797bea1b57bd
Davide Perozzi <davide@undesigned.studio>
2025-03-19 12:20:30 +0100

    feat: added lualine + markdownpreview

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,12 +16,13 @@ return {
       'vimdoc',
       'vim',
       'bash',
-      'vue'
+      'vue',
+      'yaml',
+      'pug'
     },
     auto_install = true,
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
-
   }
 }
