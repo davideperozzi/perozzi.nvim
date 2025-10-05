f3f73a571a37493564be862cfda0a210994e7302
Davide Perozzi <davide@undesigned.studio>
2025-10-05 11:56:33 +0200

    feat: dded nvim-cmp

--- init.lua
+++ init.lua
@@ -45,19 +45,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-    if client:supports_method('textDocument/completion') then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(event)
+--     local client = vim.lsp.get_client_by_id(event.data.client_id)
+--
+--     if client:supports_method('textDocument/completion') then
+--       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+--       vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+--       vim.keymap.set('i', '<C-Space>', function()
+--         vim.lsp.completion.get()
+--       end)
+--     end
+--   end
+-- })
 
 -- Include custom config
 require("custom.golang")
@@ -72,7 +72,7 @@ vim.lsp.enable({
   'rust',
   'typescript',
   'html',
-  'emmet',
+  -- 'emmet',
   'go',
   'svelte'
 })
